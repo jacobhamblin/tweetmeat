dedc1a0cac70d4a01bd689feec88168f86f885fe
Jacob Hamblin <jacob@hamblin.cc>
2020-03-22 03:04:41 -0700

    wip

--- db/index.js
+++ db/index.js
@@ -1,15 +1,15 @@
 const pg = require('pg');
 // const config = require('../config');
 
-// const dbConfig = {
-  // user: config.db.user,
-  // password: config.db.password,
-  // database: config.db.database,
-  // host: config.db.host,
-  // port: config.db.port,
-  // max: config.db.max,
-  // idleTimeoutMillis: config.db.idleTimeoutMillis,
-// };
+const dbConfig = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  max: process.env.DB_MAX,
+  idleTimeoutMillis: process.env.DB_IDLE_TIMEOUT,
+};
 
 const connectionString = process.env.DATABASE_URL;
 
