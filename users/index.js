cb12ec4332c1a67e94b1c098fe5252c879c8d3d4
Jacob Hamblin <jacob@hamblin.cc>
2020-03-24 20:20:04 -0700

    hows this

--- users/index.js
+++ users/index.js
@@ -12,11 +12,6 @@ module.exports = {
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
     }
-    User.create({
-      username: req.body.username,
-      password: req.body.password
-    }).then(user => res.json(user))
-
     const { password, username } = req.body;
 
     const sql = `INSERT INTO "user" (username, password) VALUES ('${user}', '${password}');`
