9776ed2e3f864b8b84a2198230b65ed1feb4f24d
Jacob Hamblin <jacob@hamblin.cc>
2020-03-28 18:07:11 -0700

    back to bcrypt

--- users/index.js
+++ users/index.js
@@ -1,26 +1,12 @@
 const db = require('../db');
 const { validationResult } = require('express-validator');
-const crypto = require('crypto');
+const bcrypt = require('bcrypt');
 
 module.exports = {
   login: (req, res) => {
     const { user } = req;
 
-    const { user, password } = req.body;
     res.json(user);
-    const sql = `INSERT INTO "user" (username, password) VALUES ('${username}', '${hash}');`;
-
-    db.pool.query(sql, function(err, result) {
-      // If an error occurred...
-      if (err) {
-        console.log('Error in query: ');
-        console.log(err);
-      }
-
-      console.log(result);
-      res.status(201);
-      res.send('User created');
-    });
   },
 
   create: (req, res) => {
@@ -35,8 +21,8 @@ module.exports = {
     console.log('password');
     console.log(password);
 
-    const salt = crypto.randomBytes(16).toString('hex');
-    const hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+    const saltRounds = 10
+    const hash = bcrypt.hashSync(pass, saltRounds)
 
     const sql = `INSERT INTO "user" (username, password) VALUES ('${username}', '${hash}');`;
 
