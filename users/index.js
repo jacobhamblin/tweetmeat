8b4cc9cb850910f2af31d8111dbb1a7f54149a27
Jacob Hamblin <jacob@hamblin.cc>
2020-03-28 17:46:18 -0700

    store hashed

--- users/index.js
+++ users/index.js
@@ -1,10 +1,26 @@
-const db = require('../db')
+const db = require('../db');
 const { validationResult } = require('express-validator');
+const crypto = require('crypto');
 
 module.exports = {
   login: (req, res) => {
     const { user } = req;
+
+    const { user, password } = req.body;
     res.json(user);
+    const sql = `INSERT INTO "user" (username, password) VALUES ('${username}', '${hash}');`;
+
+    db.pool.query(sql, function(err, result) {
+      // If an error occurred...
+      if (err) {
+        console.log('Error in query: ');
+        console.log(err);
+      }
+
+      console.log(result);
+      res.status(201);
+      res.send('User created');
+    });
   },
 
   create: (req, res) => {
@@ -12,25 +28,30 @@ module.exports = {
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
     }
-    console.log(req)
+    console.log(req);
     const { password, username } = req.body;
-    console.log("username")
-    console.log(username)
-    console.log("password")
-    console.log(password)
+    console.log('username');
+    console.log(username);
+    console.log('password');
+    console.log(password);
 
-    const sql = `INSERT INTO "user" (username, password) VALUES ('${username}', '${password}');`
+    const hash = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+
+    const salt = crypto.randomBytes(16).toString('hex');
+    const hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+
+    const sql = `INSERT INTO "user" (username, password) VALUES ('${username}', '${hash}');`;
 
     db.pool.query(sql, function(err, result) {
       // If an error occurred...
       if (err) {
-        console.log("Error in query: ")
+        console.log('Error in query: ');
         console.log(err);
       }
 
-      console.log(result)
-      res.status(201)
-      res.send('User created')
+      console.log(result);
+      res.status(201);
+      res.send('User created');
     });
   },
 
