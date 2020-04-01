432022c905af6625b8f6189088cba3bdb671fc5e
Jacob Hamblin <jacob@hamblin.cc>
2020-04-01 16:45:28 -0700

    ...

--- query/index.js
+++ query/index.js
@@ -8,35 +8,55 @@ module.exports = {
     const query = req.query.q;
 
     if (query) {
-      const { username, id } = req.body;
-
-      let queryID;
-      const querySQL = `SELECT id, query FROM query WHERE query=${query}`;
-      db.pool.query(querySQL, function(err, result) {
-        if (err) {
-          console.log('Error in query: ');
-          console.log(err);
-        }
-
-        if (result.rows.length > 0) {
-          const first = result.rows[0];
-          queryID = first.id;
-        } else {
-          const insertSQL = `INSERT INTO query (query) VALUES ('${query}') RETURNING id;`;
+      if (req.body) {
+        const { username, id } = req.body;
+        if (username) {
+          let queryID;
+          const querySQL = `SELECT id, query FROM query WHERE query=${query}`;
           db.pool.query(querySQL, function(err, result) {
             if (err) {
               console.log('Error in query: ');
               console.log(err);
             }
 
-            console.log('result')
-            console.log(result)
-            queryID = result;
-            console.log(queryID)
+            if (result.rows.length > 0) {
+              const first = result.rows[0];
+              queryID = first.id;
+            } else {
+              const insertSQL = `INSERT INTO query (query) VALUES ('${query}') RETURNING id;`;
+              db.pool.query(querySQL, function(err, result) {
+                if (err) {
+                  console.log('Error in query: ');
+                  console.log(err);
+                }
+
+                console.log('result')
+                console.log(result)
+                queryID = result;
+                console.log(queryID)
+              });
+
+            }
           });
 
+          // var recentQueryExists = false;
+          // const query = `SELECT id, query_id, user_id, time FROM search WHERE user_id=${id} AND query_id=${} ORDER_BY time DESC`;
+          // db.pool.query(sql, function(err, result) {
+            // if (err) {
+              // console.log('Error in query: ');
+              // console.log(err);
+            // }
+
+            // if (result.rows.length > 0) {
+              // const first = result.rows[0];
+              // if (moment(first.time).diff(moment(), 'hours') < 1) recentQueryExists = true;
+            // }
+          // });
+
+          // const insert = `insert into times (time) values (to_timestamp(${Date.now()} / 1000.0))`;
         }
-      });
+      }
+
 
       const params = {
         q: query,
@@ -62,20 +82,5 @@ module.exports = {
         next(err);
       }
     }
-    // var recentQueryExists = false;
-    // const query = `SELECT id, query_id, user_id, time FROM search WHERE user_id=${id} AND query_id=${} ORDER_BY time DESC`;
-    // db.pool.query(sql, function(err, result) {
-      // if (err) {
-        // console.log('Error in query: ');
-        // console.log(err);
-      // }
-
-      // if (result.rows.length > 0) {
-        // const first = result.rows[0];
-        // if (moment(first.time).diff(moment(), 'hours') < 1) recentQueryExists = true;
-      // }
-    // });
-
-    // const insert = `insert into times (time) values (to_timestamp(${Date.now()} / 1000.0))`;
   },
 };
