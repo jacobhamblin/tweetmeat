68afce4dfc0d23cc26cb2f7f95ec30fb22c237f0
Jacob Hamblin <jacob@hamblin.cc>
2020-04-02 00:09:18 -0700

    hows this

--- client/src/Top/index.js
+++ client/src/Top/index.js
@@ -1,18 +1,10 @@
-import React, { Component, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Top.css';
 import _sortBy from 'lodash/sortBy';
 
-export default class Top extends Component {
-  state = {
-    top: [],
-  };
-  componentDidMount() {
-    this.fetchTopQueries();
-  }
-  useEffect(() => {
-    this.fetchTopQueries();
-  })
-  fetchTopQueries = async () => {
+function Top({ updateCount }) {
+  const [top, setTop] = useState([]);
+  const fetchTopQueries = async () => {
     let url = '/api/top_queries';
     const response = await fetch(url);
     console.log('response');
@@ -21,27 +13,28 @@ export default class Top extends Component {
     top = _sortBy(top, q => Number(q.count));
     top = top.reverse();
     top = top.slice(0, 10);
-    this.setState({ top });
+    setTop(top);
   };
-  renderQuery(query) {
+  const renderQuery = query => {
     return (
       <div className="query">
         <div className="count">{query.count}</div>
         <div className="name">{query.text}</div>
       </div>
     );
-  }
-  renderTopQueries() {
-    return this.state.top.map(q => this.renderQuery(q));
-  }
-  render() {
-    const { top } = this.state;
-    console.log('hello from top');
-    return (
-      <div className="top-queries">
-        {top.length ? <h2>Top Queries</h2> : null}
-        <div className="queries">{this.renderTopQueries()}</div>
-      </div>
-    );
-  }
+  };
+  const renderTopQueries = () => {
+    return top.map(q => renderQuery(q));
+  };
+  useEffect(() => {
+    fetchTopQueries();
+  });
+  return (
+    <div className="top-queries">
+      {top.length ? <h2>Top Queries</h2> : null}
+      <div className="queries">{renderTopQueries()}</div>
+    </div>
+  );
 }
+
+export default Top;
